f1bafcbeb2dba44e67cd457ed8171c13e165947d
IDGS-804-23001528 <16sep2005.daniel@gmail.com>
2026-02-21 00:13:52 -0600

    Migraciones 21/02/2026

--- forms.py
+++ forms.py
@@ -1,25 +1,31 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
-
+ 
 from wtforms import StringField, IntegerField
 from wtforms import EmailField
 from wtforms import validators
-
+ 
 class UserForm(Form):
     id = IntegerField('id', [
         validators.number_range(min=1, max=20, message='valor no valido')
     ])
-    
+   
     nombre = StringField('nombre', [
         validators.DataRequired(message='El nombre es requerido'),
         validators.length(min=4, max=20, message='requiere min=4 max=20')
     ])
-    
-    apaterno = StringField('apaterno', [
+   
+    apellidos = StringField('apellidos', [
         validators.DataRequired(message='El apellido es requerido')
     ])
-    
+   
     email = EmailField('correo', [
         validators.DataRequired(message='El apellido es requerido'),
         validators.Email(message='Ingrese un correo valido')
     ])
+ 
+    telefono = StringField('telefono', [
+        validators.DataRequired(message='El apellido es requerido'),
+        validators.Email(message='Ingrese un telefono valido')
+    ])
+ 
--- models.py
+++ models.py
@@ -6,8 +6,13 @@ db = SQLAlchemy()
 
 class Alumnos(db.Model):
     __tablename__ = 'alumnos'
+    
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
-    apaterno = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now)
+    apellidos = db.Column(db.String(200))
+    email = db.Column(db.String(120))
+    telefono = db.Column(db.String(20))
+    created_date = db.Column(
+        db.DateTime, 
+        default=datetime.datetime.now
+    )
